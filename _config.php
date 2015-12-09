9e425b11403aee85f036c1e19b4501fff0d14e13
Andrew Rousseau <andrew@bluehousegroup.com>
2015-12-08 19:59:21 -0500

    moving code from config to proper LeftAndMainExtension #10

--- _config.php
+++ _config.php
@@ -1,8 +1,1 @@
 <?php
-
-LeftAndMain::require_css(basename(__DIR__) . '/css/custom.css');
-CMSMenu::remove_menu_item('UpgradeNotificationPage'); 
-$upgrade_notification_page = new UpgradeNotificationPage();
-if(!$upgrade_notification_page->isCurrentVersion() || ($upgrade_notification_page->isCurrentVersion() && $upgrade_notification_page->config()->ShowMenuItemWhenCurrent)) {
-	CMSMenu::add_menu_item($upgrade_notification_page->getCode(), $upgrade_notification_page->getUpgradeMessage(), 'admin/upgrade/', null, 1000);
-}
---
+++ code/UpgradeNotificationLeftAndMainExtension.php
@@ -0,0 +1,16 @@
+<?php
+
+class UpgradeNotificationLeftAndMainExtension extends LeftAndMainExtension {
+
+	public function init() {
+		parent::init();
+		Requirements::css('silverstripe-upgrade-notification/css/custom.css');
+
+		CMSMenu::remove_menu_item('UpgradeNotificationPage'); 
+		$upgrade_notification_page = new UpgradeNotificationPage();
+		if(!$upgrade_notification_page->isCurrentVersion() || ($upgrade_notification_page->isCurrentVersion() && $upgrade_notification_page->config()->ShowMenuItemWhenCurrent)) {
+			CMSMenu::add_menu_item($upgrade_notification_page->getCode(), $upgrade_notification_page->getUpgradeMessage(), 'admin/upgrade/', null, 1000);
+			error_log('here');
+		}
+	}
+}
